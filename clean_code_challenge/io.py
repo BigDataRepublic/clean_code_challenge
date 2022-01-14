a4b6b2ada755bb0b9be651e4ec6bc1d7fc2f287a
Bram Neijt <bram@neijt.nl>
2022-01-14 11:55:51 +0100

    Clean one file

--- clean_code_challenge/io.py
+++ clean_code_challenge/io.py
@@ -12,53 +12,51 @@ def read_recipes(data_path_resolver: Callable[[str], str]) -> pd.DataFrame:
     Read recipes from disk into a clean dataframe
     """
     supply_words = ["pan", "rasp", "kom"]
-    df = pd.read_csv(data_path_resolver("lunch_recipes.csv"))
+    recipes = pd.read_csv(data_path_resolver("lunch_recipes.csv"))
     for word in supply_words:
-        df[word] = df.recipe.apply(
-            lambda text: remove_punctuation(text).count(word) > 0
-        )
-        df[f"{word}"] = df[f"{word}"].apply(lambda x: x is True)
-    df["date"] = df.date.apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
-    df = df.drop(["servings", "recipe", "url", "dish"], axis=1)
+        recipes[word] = recipes.recipe.apply(lambda text: text.contains(word))
+        recipes[word] = recipes[word].apply(lambda x: x is True)
+    recipes["date"] = recipes.date.apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
+    recipes = recipes.drop(["servings", "recipe", "url", "dish"], axis=1)
 
-    return df
+    return recipes
 
 
 def read_attendance_sheet(data_path_resolver: Callable[[str], str]) -> pd.DataFrame:
     """
-    Read attendance sheet into a clean dataframe
+    Read key tag log to infer attendance sheet
     """
-    df = pd.read_csv(data_path_resolver("key_tag_logs.csv"))
-    df["timestamp2"] = df.timestamp.apply(
+    key_tag_logs = pd.read_csv(data_path_resolver("key_tag_logs.csv"))
+    key_tag_logs["timestamp"] = key_tag_logs.timestamp.apply(
         lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S")
     )
-    df["date"] = df.timestamp.apply(lambda x: datetime.strptime(x[:10], "%Y-%m-%d"))
-    df["time"] = df.timestamp2.apply(lambda x: x.time())
-    df["timestamp"] = df["timestamp2"]
-    df = df.drop("timestamp2", axis=1)
+    key_tag_logs["date"] = key_tag_logs.timestamp.apply(
+        lambda x: datetime.strptime(x[:10], "%Y-%m-%d")
+    )
+    key_tag_logs["time"] = key_tag_logs.timestamp.apply(lambda x: x.time())
 
-    result = pd.DataFrame(np.array(df.date), columns=["date"]).drop_duplicates()
+    attendance_sheet = pd.DataFrame(
+        np.array(key_tag_logs.date), columns=["date"]
+    ).drop_duplicates()
 
-    for name in df.name.unique():
-        lunchdates = []
-        for datum in df.date.unique():
-            df2 = df[df.name == name]
-            df2 = df2[df2.date == datum]
+    for name in key_tag_logs.name.unique():
+        lunch_dates = []
+        for date in key_tag_logs.date.unique():
+            personal_day_log = key_tag_logs[key_tag_logs.name == name]
+            personal_day_log = personal_day_log[personal_day_log.date == date]
 
-            dataframe_check_in = df2[df2.event == "check in"]
-            dataframe_check_in = dataframe_check_in[
-                dataframe_check_in.time < time(12, 0, 0)
-            ]
+            check_in_events = personal_day_log[personal_day_log.event == "check in"]
+            check_in_events = check_in_events[check_in_events.time < time(12, 0, 0)]
 
-            df_check_out = df2[df2.event == "check out"]
-            df_check_out = df_check_out[df_check_out.time > time(12, 0, 0)]
-            if df_check_out.shape[0] > 0 and dataframe_check_in.shape[0] > 0:
-                lunchdates.append(datum)
+            check_out_events = personal_day_log[personal_day_log.event == "check out"]
+            check_out_events = check_out_events[check_out_events.time > time(12, 0, 0)]
+            if check_out_events.shape[0] > 0 and check_in_events.shape[0] > 0:
+                lunch_dates.append(date)
 
-        result[f"{name}"] = result.date.apply(
-            lambda x: 1 if x in list(lunchdates) else 0
+        attendance_sheet[name] = attendance_sheet.date.apply(
+            lambda x: 1 if x in list(lunch_dates) else 0
         )
-    return result
+    return attendance_sheet
 
 
 def read_diswasher_log(data_path_resolver: Callable[[str], str]) -> pd.DataFrame:
