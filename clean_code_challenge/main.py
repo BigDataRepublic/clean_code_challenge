50475ea294f296c24bc06adf04d1a4e7a499509e
Bram Neijt <bram@neijt.nl>
2022-01-14 11:59:27 +0100

    Clean

--- clean_code_challenge/main.py
+++ clean_code_challenge/main.py
@@ -1,7 +1,6 @@
 from typing import List, Callable
 from datetime import datetime, time
 
-import pandas as pd
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import os
@@ -22,14 +21,14 @@ def train_model() -> dict:
     attendance = read_attendance_sheet(data_path_resolver)
     dishwasher_log = read_diswasher_log(data_path_resolver)
 
-    df = (
+    training_data = (
         recipes.merge(attendance, on="date", how="outer")
         .merge(dishwasher_log)
         .fillna(0)
     )
-    reg = LinearRegression(fit_intercept=False, positive=True).fit(
-        df.drop(["dishwashers", "date"], axis=1), df["dishwashers"]
-    )
+    train_X = training_data.drop(["dishwashers", "date"], axis=1)
+    train_Y = training_data["dishwashers"]
+    reg = LinearRegression(fit_intercept=False, positive=True).fit(train_X, train_Y)
     return dict(zip(reg.feature_names_in_, [round(c, 3) for c in reg.coef_]))
 
 
