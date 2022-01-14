ba81dd3857e28dffef85a6a3ecd8fc0b5e820f8a
Bram Neijt <bram@neijt.nl>
2022-01-14 11:43:15 +0100

    Move code

--- clean_code_challenge/config.py
+++ clean_code_challenge/config.py
@@ -7,7 +7,7 @@ def get_data_path_resolver(project_data_path: str = "data") -> Callable[[str], s
     Return a method that allows for resolving a full data path given a filename
     """
 
-    def _get_data_path(filename: str) -> str:
+    def data_path_resolver(filename: str) -> str:
         return os.path.join(project_data_path, filename)
 
-    return _get_data_path
+    return data_path_resolver
